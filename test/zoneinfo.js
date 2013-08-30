d50d5aff6092c9970fa009f9783c9304ceb797d8
Joey Dong <joeydong@whitepixel.us>
2013-08-30 11:45:55 -0400

    Updated zoneinfo and tests

--- test/zoneinfo.js
+++ test/zoneinfo.js
@@ -4,34 +4,20 @@ var _ = require('underscore');
 var zoneinfo = require('../lib/zoneinfo');
 
 describe('lib/zoneinfo.js', function() {
-  it('should load 10 continents', function() {
-    assert.ok(zoneinfo['Africa']);
-    assert.ok(zoneinfo['America']);
-    assert.ok(zoneinfo['Antarctica']);
-    assert.ok(zoneinfo['Arctic']);
-    assert.ok(zoneinfo['Asia']);
-    assert.ok(zoneinfo['Atlantic']);
-    assert.ok(zoneinfo['Australia']);
-    assert.ok(zoneinfo['Europe']);
-    assert.ok(zoneinfo['Indian']);
-    assert.ok(zoneinfo['Pacific']);
-  });
-
-  it('should load matching ICS for zones', function() {
-    var checkICS = function(zone) {
-      _.each(zone, function(zoneICS, zoneName) {
-        if (zoneName !== '.DS_Store') {
-          if (_.isArray(zoneICS)) {
-            // ics lines
-            assert.equal(zoneName, _.last(zoneICS[1].split('/')));
-          }
-          else {
-            // folder
-            checkICS(zoneICS);
-          }
+  it('should load matching ICS definition for zoneinfo object', function() {
+    var checkICSDefinition = function(zone) {
+      _.each(zone, function(zoneDefinition, zoneName) {
+        if (_.isArray(zoneDefinition)) {
+          // check definition
+          var tzid = _.last(zoneDefinition[1].split(':'));
+          assert.equal(zoneName, _.last(tzid.split('/')));
+        }
+        else {
+          checkICSDefinition(zoneDefinition);
         }
       });
     };
-    checkICS(zoneinfo);
+
+    checkICSDefinition(zoneinfo);
   });
 });
