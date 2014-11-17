2f72adbf80722751d15294120f2a8df37283c12e
Pierre-Élie Fauché <pe@sunrise.am>
2014-11-17 18:51:30 -0500

    Escape/Unescape Person’s CN

--- test/vobject/person.js
+++ test/vobject/person.js
@@ -23,11 +23,17 @@ describe('lib/vobject/person.js', function() {
   });
 
   describe('setCN', function() {
-    it('should set CN', function(done) {
+    it('should set CN (escaped)', function(done) {
       var person = VObject.person();
+
+      person.escape = function(str) {
+        assert.deepEqual(str, 'value');
+        return 'escaped value';
+      };
+
       person.setParameter = function(name, value) {
         assert.equal(name, 'CN');
-        assert.equal(value, 'value');
+        assert.equal(value, 'escaped value');
         done();
       };
       person.setCN('value');
@@ -35,10 +41,21 @@ describe('lib/vobject/person.js', function() {
   });
 
   describe('getCN', function() {
-    it('should get CN', function() {
+    it('should default tom undefined', function() {
       var person = VObject.person();
+      assert.equal(person.getCN(), undefined);
+    });
+
+    it('should get CN (unescaped)', function() {
+      var person = VObject.person();
+
+      person.unescape = function(str) {
+        assert.deepEqual(str, 'value');
+        return 'unescaped value';
+      };
+
       person.setCN('value');
-      assert.equal(person.getCN(), 'value');
+      assert.equal(person.getCN(), 'unescaped value');
     });
   });
 
